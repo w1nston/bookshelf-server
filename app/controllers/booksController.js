fd3be3a093b866b9a61e0d7a9be6e5ddb1acc49f
w1nston <wallander.jonas@gmail.com>
2016-07-27 15:15:28 +0200

    Remove unnecessary logging in controller.

--- app/controllers/booksController.js
+++ app/controllers/booksController.js
@@ -8,13 +8,11 @@ module.exports = function BooksController(app) {
     index(request, response) {
       bookModel.findAll((error, books) => {
         if (error) {
-          logger.error('Server responded with status 500');
           logger.error('Internal error', error);
           return response
             .status(500)
             .json(error);
         }
-        logger.info('Response status: 200');
         return response
           .status(200)
           .json(books);
@@ -24,13 +22,11 @@ module.exports = function BooksController(app) {
     create(request, response) {
       bookModel.create(request.body, error => {
         if (error) {
-          logger.error('Server responded with status 500');
           logger.error('Internal error:', error);
           return response
             .status(500)
             .send(error);
         }
-        logger.info('Server responded with status 201');
         return response
           .status(201)
           .send({ message: 'BookModel added!' });
